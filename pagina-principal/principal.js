2fb56c0a77bd955d8ac4a353259f1cd37ec6cf99
Levi-Mid <abdomlevi1999@gmail.com>
2025-10-06 17:56:50 -0300

    edições para adição do carrossel

---
+++ pagina-principal/principal.js
@@ -0,0 +1,17 @@
+const carrosselImages = document.querySelector('.carrosselImagens');
+const images = document.querySelectorAll('.carrosselImagens img');
+let index = 0;
+
+// Função para trocar de imagem
+function slideImages() {
+  index++;
+  if (index >= images.length) {
+    index = 0;
+  }
+
+  // Move o carrossel horizontalmente
+  carrosselImages.style.transform = `translateX(${-index * 100}%)`;
+}
+
+// Troca a cada 3 segundos
+setInterval(slideImages, 15000);
